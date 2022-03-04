42379daf4f41cca700fa2a53481f1032b2f5f444
laconfianceensoi <eioancoraimparo@gmail.com>
2022-03-05 04:31:49 +0900

    Updated index.html files

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -39,7 +39,7 @@ let clickChangeFruit = true;
 //let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '이디야 딸기라떼색 손진욱을 잡아라! 아이돌 수박게임';
+document.getElementsByTagName("title")[0].innerText = '이디야 레몬에이드색 손진욱을 잡아라! 아이돌 레몬수박게임';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
